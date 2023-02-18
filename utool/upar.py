e038071034f3286785931800889aecc002dbfe82
bob chase <bob.chase@gmail.com>
2023-02-18 10:01:56 -0500

    refactor upar

--- utool/upar.py
+++ utool/upar.py
@@ -7,52 +7,42 @@ import re
 def paragraph(data, columns=80, indent=None):
     """yield lines from data that are no more than columns chars long"""
 
-    line = ""
-    top = True
-    new_paragraph = False
-
-    # pylint: disable=too-many-nested-blocks
+    paragraphs = []
+    para = []
     for data_line in data.split("\n"):
         if len(data_line.rstrip()) == 0:
-            if not top:
-                if line:
-                    if new_paragraph:
-                        yield ""
-                        new_paragraph = False
-                    yield f"{' ' * indent}{line}"
-                    line = ""
-                new_paragraph = True
-        else:
-            if top:
-                if indent is None:
-                    indent = len(re.match(r"(\s*)", data_line).group(1))
-                top = False
-                maxlen = columns - indent
-            for word in data_line.split():
-                if not line:
-                    if len(word) >= maxlen:
-                        if new_paragraph:
-                            yield ""
-                            new_paragraph = False
-                        yield f"{' ' * indent}{word}"
-                    else:
-                        line = word
+            if para:
+                paragraphs.append(para)
+                para = []
+            continue
 
-                elif len(newl := f"{line} {word}") > maxlen:
-                    if new_paragraph:
-                        yield ""
-                        new_paragraph = False
-                    yield f"{' ' * indent}{line}"
-                    line = word
+        if indent is None:
+            indent = len(re.match(r"(\s*)", data_line).group(1))
 
-                else:
-                    line = newl
+        for word in data_line.split():
+            para.append(word)
 
-    if line:
-        if new_paragraph:
+    if para:
+        paragraphs.append(para)
+
+    maxlen = columns - (indent or 0)
+    line = ""
+    for paragraph_no, para in enumerate(paragraphs):
+        if paragraph_no > 0:
             yield ""
-            new_paragraph = False
-        yield f"{' ' * indent}{line}"
+        for word in para:
+            if not line and len(word) > maxlen:
+                yield f"{' ' * indent}{word}"
+            elif not line:
+                line = word
+            elif len(newline := f"{line} {word}") > maxlen:
+                yield f"{' ' * indent}{line}"
+                line = word
+            else:
+                line = newline
+        if line:
+            yield f"{' ' * indent}{line}"
+            line = ""
 
 
 if __name__ == "__main__":
