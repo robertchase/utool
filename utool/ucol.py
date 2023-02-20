7cdaa11c4c9d2d09048f723808c7401bc4fb03cd
bob chase <bob.chase@gmail.com>
2023-02-20 09:01:19 -0500

    lint and black ucol

--- utool/ucol.py
+++ utool/ucol.py
@@ -3,21 +3,25 @@
 import re
 
 
+class UcolException(Exception):
+    """ucol specific exception"""
+
+
 def split(data, indexes, delimiter=" ", out_delimiter=" ", nullable=False):
     """split text into columns
 
-       data - open file
-       indexes - list of column numbers to extract from each line in data
-                 these can be:
-                     integer (first column is "1", second is "2", etc)
-                     negative integer (count from right)
-                     <integer>+ (all columns including and after "integer")
-                 columns can repeat and be in any order
-       delimiter - separator between input columns
-                   multiple sequential delimiters will result in multiple
-                   empty columns unless nullable=True
-       out_delimiter - separator between output columns
-       nullable - control parsing of multiple delimiters (see delimiter)
+    data - open file
+    indexes - list of column numbers to extract from each line in data
+              these can be:
+                  integer (first column is "1", second is "2", etc)
+                  negative integer (count from right)
+                  <integer>+ (all columns including and after "integer")
+              columns can repeat and be in any order
+    delimiter - separator between input columns
+                multiple sequential delimiters will result in multiple
+                empty columns unless nullable=True
+    out_delimiter - separator between output columns
+    nullable - control parsing of multiple delimiters (see delimiter)
     """
 
     _indexes = []
@@ -29,8 +33,7 @@ def split(data, indexes, delimiter=" ", out_delimiter=" ", nullable=False):
             try:
                 index = int(index)
             except ValueError as exc:
-                raise Exception(
-                    f"column number ({index}) must be numeric") from exc
+                raise UcolException(f"column number ({index}) must be numeric") from exc
             if index > 0:
                 index -= 1
             _indexes.append(index)
@@ -50,9 +53,9 @@ def split(data, indexes, delimiter=" ", out_delimiter=" ", nullable=False):
                 else:
                     result += cols[index]
             except IndexError as exc:
-                raise Exception(
-                    f"line {lineno}:'{line}'"
-                    f" does not have {index} columns") from exc
+                raise UcolException(
+                    f"line {lineno}:'{line}'" f" does not have {index} columns"
+                ) from exc
         yield result
 
 
@@ -61,19 +64,30 @@ if __name__ == "__main__":
     import sys
 
     parser = argparse.ArgumentParser(description="select columns from text")
-    parser.add_argument("--delimiter", "-d", default=" ",
-                        help="input column delimiter, default=' '")
-    parser.add_argument("--output-delimiter", "-D", default=" ",
-                        help='output column delimiter, default=" "')
-    parser.add_argument("--null-columns", "-n", action="store_true",
-                        help="ignore consecutive empty columns")
-    parser.add_argument("columns", nargs="+",
-                        help="list of column numbers, e.g. 1 2 -1 5+")
+    parser.add_argument(
+        "--delimiter", "-d", default=" ", help="input column delimiter, default=' '"
+    )
+    parser.add_argument(
+        "--output-delimiter",
+        "-D",
+        default=" ",
+        help='output column delimiter, default=" "',
+    )
+    parser.add_argument(
+        "--null-columns",
+        "-n",
+        action="store_true",
+        help="ignore consecutive empty columns",
+    )
+    parser.add_argument(
+        "columns", nargs="+", help="list of column numbers, e.g. 1 2 -1 5+"
+    )
     args = parser.parse_args()
     for response in split(
-            sys.stdin.read(),
-            args.columns,
-            args.delimiter,
-            args.output_delimiter,
-            args.null_columns):
+        sys.stdin.read(),
+        args.columns,
+        args.delimiter,
+        args.output_delimiter,
+        args.null_columns,
+    ):
         print(response)
